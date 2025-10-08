f2afa288d721c0a9ac3f344c0d1088e3eef6771e
Luana <luanaferreirasilva2006@gmail.com>
2025-10-08 08:06:40 -0300

    testa conexão com o banco

---
+++ backend/connectionFactory/connectionFactory.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("metrodb", "root", "19282322Lfs#", {
+    host : "localhost",
+    dialect : "mysql"
+});
+
+sequelize.authenticate()
+.then(function(){
+    console.log("Conexão com o banco de dados realizada com sucesso!");
+}).catch(function(){
+    console.log("Erro: a conexão com o banco de dados falhou.")
+});
+
+module.exports = sequelize;
+
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const db = require("backend/connectionFactory/connectionFactory.js");
+
 app.get('/', (req, res) => {
     res.send("Olá, devs!")
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na http://localhost:${PORT}/`)
+    console.log(`Servidor rodando em http://localhost:${PORT}/`)
 });
