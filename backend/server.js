cd5f90a019c4310dfed5a0c26ac196e58843fbe4
Luana <luanaferreirasilva2006@gmail.com>
2025-10-13 08:24:57 -0300

    feat: adiciona comando para fazer uso do json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+app.use(express.json()) // testar isso 
 
 const db = require("./connectionFactory/connectionFactory.js");
 
