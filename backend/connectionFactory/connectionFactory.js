133005739de4f7992523428285ceb88b4b5a418b
Luana <luanaferreirasilva2006@gmail.com>
2025-10-19 15:02:07 -0300

    feat: isola credenciais do MySQL em variáveis de ambiente.

--- backend/connectionFactory/connectionFactory.js
+++ backend/connectionFactory/connectionFactory.js
@@ -1,15 +1,25 @@
+require('dotenv').config({path: '../.env'});
+
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("metrodb", "root", "19282322Lfs#", {
-    host : "localhost",
+
+const DB_USER = process.env.DB_USER;
+const DB_NAME = process.env.DB_NAME;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+
+console.log(`Tentando conectar com o usuário ${DB_USER} e senha ${DB_PASS}`)
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host : DB_HOST,
     dialect : "mysql"
 }); // fazer conexão com bd na nuvem também
 
 sequelize.authenticate()
 .then(function(){
     console.log("Conexão com o banco de dados realizada com sucesso!");
-}).catch(function(){
+}).catch((err) => {
     console.log("Erro: a conexão com o banco de dados falhou.")
+    console.log(err.message);
 });
 
 module.exports = sequelize;
-
