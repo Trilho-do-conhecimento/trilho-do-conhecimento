1a0b4faabdb8b101597dd6d1c7077825dc25b028
Thiago <thiagosm2006@gmail.com>
2025-11-04 13:03:39 -0300

    feat: criar service para página de criação da lista de presença

---
+++ backend/services/criarListaPresencaService.js
@@ -0,0 +1,32 @@
+const ListaPresencaDAO = require('../DAO/ListaPresencaDAO');
+const ListaPresencaUsuarioDAO = require('../DAO/ListaPresencaUsuarioDAO');
+
+class ListaDePresencaService {
+  static async criar(body) {
+    return await ListaPresencaDAO.criar(body);
+  }
+
+  static async buscarTodas() {
+    return await ListaPresencaDAO.buscarTodos({ includeAlunos: true });
+  }
+
+  static async buscarPorId(id) {
+    return await ListaPresencaDAO.buscarPorId(id, { includeAlunos: true });
+  }
+
+  static async atualizar(id, body) {
+    return await ListaPresencaDAO.atualizar(id, body);
+  }
+
+  static async atualizarStatusAluno(id, id_usuario, status_assinatura) {
+    return await ListaPresencaUsuarioDAO.atualizarStatus(id, id_usuario, status_assinatura);
+  }
+
+  static async deletar(id) {
+    return await ListaPresencaDAO.deletar(id);
+  }
+}
+
+module.exports = ListaDePresencaService;
+
+
