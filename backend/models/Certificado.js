f58b4d27799c0d9d88c3d87f5db838ca63ab5fcd
Thiago <thiagosm2006@gmail.com>
2025-11-13 21:33:25 -0300

    refactor: adicionar novas informações da entidade certificado

--- backend/models/Certificado.js
+++ backend/models/Certificado.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../connectionFactory/connectionFactory.js');
-const Usuario = require('./Usuario.js'); // importar model Usuario
+const Usuario = require('./Usuario.js');
 
 const Certificado = sequelize.define('Certificado', {
     id_certificado: {
@@ -8,18 +8,21 @@ const Certificado = sequelize.define('Certificado', {
         primaryKey: true,
         autoIncrement: true
     },
-    certificador1: {
+
+    // Dados do certificador
+    certificador: {
         type: DataTypes.STRING(100),
         allowNull: false
     },
-    registro_c1: {
+    registro_certificador: {
         type: DataTypes.STRING(100),
         allowNull: false
     },
-    cargo_c1: {
+    cargo_certificador: {
         type: DataTypes.STRING(100),
         allowNull: false
     },
+
     id_concluinte: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -28,6 +31,8 @@ const Certificado = sequelize.define('Certificado', {
             key: 'id_usuario'
         }
     },
+
+    // Dados do certificado
     nome_certificado: {
         type: DataTypes.STRING(100),
         allowNull: false
@@ -35,10 +40,37 @@ const Certificado = sequelize.define('Certificado', {
     descricao: {
         type: DataTypes.TEXT,
         allowNull: false
+    },
+
+    nivel: {
+        type: DataTypes.ENUM('iniciante', 'intermediario', 'avancado'),
+        allowNull: false
+    },
+    tabela: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    normas: {
+    type: DataTypes.TEXT,
+    allowNull: false
+    },
+    cidade: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    data_certificado: {
+        type: DataTypes.DATE,
+        allowNull: false
     }
 }, {
     tableName: 'certificado',
     timestamps: false
+});
+
+// Associação (caso haja alguma dificuldade analizar o CertificadoDAO.js)
+Certificado.belongsTo(Usuario, {
+    as: 'concluinte',
+    foreignKey: 'id_concluinte'
 });
 
 module.exports = Certificado;
